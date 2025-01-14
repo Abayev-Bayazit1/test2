db85c2ae8c25cbc948467ebaa11207becbea5fa8
Abayev Bayazit <240918@astanait.edu.kz>
2025-01-14 19:37:07 +0500

    add Square

---
+++ src/models/Square.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Square  implements Ishape{
+    public int x;
+
+
+    public Square() {}
+
+    public Square(int x) {
+            setX(x);
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+
+    @Override
+    public double perimeter() {
+        return 4 * x;
+    }
+
+    @Override
+    public double area() {
+        return x * x;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Square: " + "The area of the square is " + area() + " and the perimeter is " + perimeter();
+    }
+}
